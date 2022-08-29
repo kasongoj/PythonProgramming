2ff7fab256703bb63cb4d0c0f0f941973742444f
kasongoj <111376499+kasongoj@users.noreply.github.com>
2022-08-29 08:32:13 -0500

    Update first.py

--- first.py
+++ first.py
@@ -1,5 +1,3 @@
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
 # Programmer: Joseph Kasongo
 # Program: Printing hello world
 # Version: Python 3.8.2
