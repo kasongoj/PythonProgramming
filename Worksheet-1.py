112b96198c6cddc788750fd708f5b93a9f29c22a
kasongoj <111376499+kasongoj@users.noreply.github.com>
2022-09-01 21:51:59 -0500

    Add files via upload

---
+++ Worksheet-1.py
@@ -0,0 +1,33 @@
+# Programmer: Joseph Kasongo
+# Program: Python Worksheet-1
+# Version: Python 3.8.2
+# First written: 09/01/2022, 09:48:00 PM
+# # Last changed: 09/01/2022, 09:48:00 PM
+print ('Python Lab 1a: The print function, Unicode values')
+print ('\t')
+print ('Joseph Kasongo')
+print ('Lubumbashi')
+print (4 * 11 * 29)
+print (403 // 12)
+print (403 % 12)
+favNumber = 11
+favMonth = 'February'
+print (favNumber)
+print (favMonth)
+print ('\u03a3' + ' Greek Capital Letter Sigma')
+print ('\t')
+print ('Python Lab 1b: Binary, Decimal, Hex')
+print ('\t')
+print (11, 29*4, 'Favorite Numbers')
+x = 29
+print (x, bin(x), hex(x)) 
+y = 0b100
+z = 0x1d
+print (y, z)
+w = x + y + z
+print ('the sum is ' , w)
+print (11, 29,'four', sep=',')
+print (0, 1,'two', sep=' ')
+
+
+
