b0ac976c25d619158cf9075a35573c2e889f19bc
kasongoj <Joseph.Kasongo@sckans.edu>
2022-09-21 08:45:59 -0500

    added alternate approach

--- oddNumber.py
+++ oddNumber.py
@@ -3,8 +3,13 @@
 # created on: 09/21/2022 08:09:00
 # last modified: 09/21/2022 08:09:00
 # version:1.0.0
-print("Odd Numbers From the First 100 Natural Numbers:")
-for i in range(0,101):
-    if (i%2 != 0):
-        print(i, end=" ")
+
+# print("Odd Numbers From the First 100 Natural Numbers:")
+# for i in range(0,101):
+#     if(i%2 != 0):
+#         print(i, end=" \n")
+
+print("Alternate Approach")
+for k in range(1,101):
+    print(((2*k)-1), end=" ")
 
