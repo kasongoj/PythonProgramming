918e0c8405c660dbfee21b7686e4969c2ab6ac48
kasongoj <Joseph.Kasongo@sckans.edu>
2022-09-21 08:21:00 -0500

    Odd Numbers

---
+++ oddNumber.py
@@ -0,0 +1,10 @@
+# programmer: Joseph Kasongo
+# program: Odd Numbers From First 100 Natural Numbers
+# created on: 09/21/2022 08:09:00
+# last modified: 09/21/2022 08:09:00
+# version:1.0.0
+print("Odd Numbers From the First 100 Natural Numbers:")
+for i in range(0,101):
+    if (i%2 != 0):
+        print(i, end=" ")
+
