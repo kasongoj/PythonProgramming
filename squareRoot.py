ec08a6104883470c6a9344ee8e82d9119adfb4fc
kasongoj <Joseph.Kasongo@sckans.edu>
2022-09-23 09:45:31 -0500

    sqaure root

---
+++ squareRoot.py
@@ -0,0 +1,10 @@
+# programmer: Joseph Kasongo
+# program: Odd Numbers From First 100 Natural Numbers
+# created on: 09/23/2022 09:26:00
+# last modified: 09/23/2022 09:26:00
+# version:1.0.0
+import math
+print("Printing square root of first 100 natural numbers")
+for i in range(1,101):
+         print(math.sqrt(i), end=' ')
+
