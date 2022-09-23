11ae57c8e96eb94b7b81bbaaeb43657ebe8e226e
kasongoj <Joseph.Kasongo@sckans.edu>
2022-09-23 09:21:37 -0500

    reverse odd numbers with While Loop

--- oddNumber.py
+++ oddNumber.py
@@ -16,16 +16,22 @@
 #         print("\n")
 #     print(((2*k)-1), end=" ")
 
-print("Printing odd numbers from first 100 natural numbers using While Loop")
-i=1
-# When I change to <100, I get up to 199
-while (i<51):
-    print(((2*i)-1), end=' ')
-    i = i+1
-print("\n")
-print("Alternate way of printing odd numbers from first 100 natural numbers using While Loop")
-i=1
-while i<=100:
-    if(i%2!=0):
-        print(i,end=' ')
-    i=i+1
+# print("Printing odd numbers from first 100 natural numbers using While Loop")
+# i=1
+# # When I change to <100, I get up to 199
+# while (i<51):
+#     print(((2*i)-1), end=' ')
+#     i = i+1
+# print("\n")
+# print("Alternate way of printing odd numbers from first 100 natural numbers using While Loop")
+# i=1
+# while i<=100:
+#     if(i%2!=0):
+#         print(i,end=' ')
+#     i=i+1
+
+i=100
+while i>=1:
+    if (i%2!=0):
+        print(i, end=' ')
+    i=i-1
