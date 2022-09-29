df69e165dbb3cf1c97e7f717f14f3e4c360271fc
kasongoj <Joseph.Kasongo@sckans.edu>
2022-09-29 00:04:23 -0500

    assignment2 codes

--- assignmentTwo.py
+++ assignmentTwo.py
@@ -29,4 +29,29 @@ choice=input("Enter operation number: ")
 while choice!="1" and choice!="2" and choice!="3" and choice!="4" and choice!="5":
     print("Invalid choice, enter again")
     choice=input("Enter the right operation number: ")
-print("We move onto the next step")
+if choice=="1":
+    print("Provide the value of the radius\n")
+    radius=int(input("radius= "))
+    areaCircle=math.pi * radius * radius
+    print("Area of the circle is ",areaCircle)
+elif choice=="2":
+    print("Provide the value of the width and the height\n")
+    width=int(input("width= "))
+    height=int(input("height= "))
+    areaRect=width*height
+    print("Area of the rectangle is ",areaRect)
+elif choice=="3":
+    print("Provide the value of the base and the height\n")
+    base=int(input("base= "))
+    height=int(input("height= "))
+    areaTr=base*height*0.5
+    print("Area of the triangle is ",areaTr)
+elif choice=="4":
+    print("Provide the value of the diagonals\n")
+    diagFirst=int(input("diagonal1= "))
+    diagTwo=int(input("diagonal2= "))
+    areaRh=diagFirst*diagTwo*0.5
+    print("Area of the rhombus is ",areaRh)
+else:
+    number=int(input("Enter a number: "))
+    print(f"The square root of {number} is ",math.sqrt(number))
