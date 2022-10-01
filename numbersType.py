6694829114a85a5864e42d7272ee555cabc84317
kasongoj <Joseph.Kasongo@sckans.edu>
2022-09-30 21:40:39 -0500

    types of numbers

--- numbersType.py
+++ numbersType.py
@@ -14,7 +14,7 @@ print("1. Odd Numbers\n"
       "5. Palindrome Numbers")
 choice=int(input("Select the operation by choosing a number:\t"))
 if choice==1:
-    print("Odd Numbers From the First 100 Natural Numbers:")
+    print("\nOdd Numbers From the First 100 Natural Numbers:")
     for i in range(0,101):
         if(i%2 != 0):
             print(i, end=" ")
@@ -26,38 +26,38 @@ if choice==1:
     #         print("\n")
     #         print(((2*k)-1), end=" ")
 
-    print("\nPrinting odd numbers from first 100 natural numbers using While Loop")
+    print("\nPrinting odd numbers from first 100 natural numbers using While Loop:")
     i=1
     # When I change to <100, I get up to 199
     while (i<51):
         print(((2*i)-1), end=' ')
         i = i+1
-# print("\n")
-# print("Alternate way of printing odd numbers from first 100 natural numbers using While Loop")
-# i=1
-# while i<=100:
-#     if(i%2!=0):
-#         print(i,end=' ')
-#     i=i+1
 
-# i=100
-# while i>=1:
-#     if (i%2!=0):
-#         print(i, end=' ')
-#     i=i-1
+    print("\nAlternate way of printing odd numbers from first 100 natural numbers using While Loop:")
+    i=1
+    while i<=100:
+        if(i%2!=0):
+            print(i,end=' ')
+        i=i+1
+    print("\nReverse Printing odd numbers from first 100 natural numbers using While Loop:")
+    i=100
+    while i>=1:
+        if (i%2!=0):
+            print(i, end=' ')
+        i=i-1
 elif choice==2:
-    print("Printing even numbers using For Loop")
+    print("\nPrinting even numbers using For Loop:")
     for i in range(2,101):
         if i%2==0:
             print(i, end=' ')
-    print("\nPrinting even numbers using While Loop")
+    print("\nPrinting even numbers using While Loop:")
     i=2
     while i<=100:
         if i%2==0:
             print(i, end=' ')
         i +=1
 elif choice==3:
-    print("\nPrinting prime numbers from first 100 natural numbers")
+    print("\nPrinting prime numbers from first 100 natural numbers:")
     for i in range(1,101):      # iterating for numbers between 2 and n/2+1
         counter=0     # initializing counter variable to 0
         for j in range(2,int(i/2)+1):       # checking number of divisors for a number
@@ -66,7 +66,7 @@ elif choice==3:
         if counter==0:          # decision to see if counter is 0 i.e no divisors
             print(i, end=' ')   # printing if the number is prime
 elif choice==4:
-    print("\nPrinting perfect numbers from first 100 natural numbers")
+    print("\nPrinting perfect numbers from first 100 natural numbers:")
     for i in range(1, 51):
         sum=0
         for j in range(1,i):
@@ -74,8 +74,8 @@ elif choice==4:
                 sum+=j
         if sum==i:
             print(i,end=" ")
-else:
-    print("Palindrome numbers from first 100 natural numbers")
+elif choice==5:
+    print("\nCheck if a number is palindrome:")
     number=int(input("Enter a number: "))
     originalNumber=number
     rev=0
@@ -87,9 +87,5 @@ else:
         print("Is a palindrome")
     else:
         print("Is not a palindrome")
-
-
-
-
-
-
+else:
+    print(f"Sorry, Option {choice} does not exist\nTry again")
