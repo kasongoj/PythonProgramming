70b67166f6149861b6f2b49941cbe0d8f9803a3e
kasongoj <Joseph.Kasongo@sckans.edu>
2022-11-11 09:19:12 -0600

    edit

--- others/logical.py
+++ others/logical.py
@@ -1,1 +1,9 @@
+""""
+programmer: Joseph Kasongo
+program: assignment 2
+python version: Python 3.10.7
+created on: 09/24/2022 09:36:00 PM
+last modified: 11/1/2022 09:17:00 AM
+
+"""
 print("Hello World")
