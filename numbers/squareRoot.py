d46750fb13a60cf66a77fe4311d9fc843c0ccd99
kasongoj <Joseph.Kasongo@sckans.edu>
2022-11-11 09:07:40 -0600

    square root

--- numbers/squareRoot.py
+++ numbers/squareRoot.py
@@ -2,7 +2,7 @@
 # program: Odd Numbers From First 100 Natural Numbers
 # created on: 09/23/2022 09:26:00
 # last modified: 09/23/2022 09:26:00
-# version:1.0.0
+# version:Python 3.10.7
 import math
 print("Printing square root of first 100 natural numbers")
 for i in range(1,101):
