bd39fedd5bb5db7c18abd7a19997cadd25a09c3c
kasongoj <Joseph.Kasongo@sckans.edu>
2022-11-11 09:02:40 -0600

    intro to loopsd

--- numbers/Loops.py
+++ numbers/Loops.py
@@ -1,7 +1,8 @@
+#introduction to loops
 print("Odd Numbers From the First 100 Natural Numbers:")
 for i in range(0,101):
     if(i%2!= 0):
-        print(i, end=' ')
+        print(i, end=' ') #printing on same line
 
 
 
